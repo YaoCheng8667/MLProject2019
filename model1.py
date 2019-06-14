ac9ca48f37d10d4c6b40114daa3bd7849201f63f
“YC <951766143@qq.com>
2019-06-14 17:38:29 +0800

    first commit

---
+++ model1.py
@@ -0,0 +1,191 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+import cv2
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import os
+from PIL import Image
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import load_img
+from keras.utils import np_utils
+
+
+# In[2]:
+
+
+parasitized_data = os.listdir('./data/Parasitized/') 
+print(parasitized_data[:10])
+uninfected_data = os.listdir('./data/Uninfected/')
+print('\n')
+print(uninfected_data[:10])
+
+
+# In[3]:
+
+
+data = []
+labels = []
+for img in parasitized_data:
+    try:
+        img_read = plt.imread('./data/Parasitized/' + "/" + img)
+        img_resize = cv2.resize(img_read, (50, 50))
+        img_array = img_to_array(img_resize)
+        data.append(img_array)
+        labels.append(1)
+    except:
+        None
+        
+for img in uninfected_data:
+    try:
+        img_read = plt.imread('./data/Uninfected' + "/" + img)
+        img_resize = cv2.resize(img_read, (50, 50))
+        img_array = img_to_array(img_resize)
+        data.append(img_array)
+        labels.append(0)
+    except:
+        None
+
+
+# In[4]:
+
+
+plt.imshow(data[0])
+plt.show()
+
+
+# In[5]:
+
+
+image_data = np.array(data)
+labels = np.array(labels)
+
+
+# In[6]:
+
+
+idx = np.arange(image_data.shape[0])
+np.random.shuffle(idx)
+image_data = image_data[idx]
+labels = labels[idx]
+
+
+# In[7]:
+
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(image_data, labels, test_size = 0.2, random_state = 101)
+
+
+# In[8]:
+
+
+y_train = np_utils.to_categorical(y_train, num_classes = 2)
+y_test = np_utils.to_categorical(y_test, num_classes = 2)
+
+
+# In[9]:
+
+
+print(f'SHAPE OF TRAINING IMAGE DATA : {x_train.shape}')
+print(f'SHAPE OF TESTING IMAGE DATA : {x_test.shape}')
+print(f'SHAPE OF TRAINING LABELS : {y_train.shape}')
+print(f'SHAPE OF TESTING LABELS : {y_test.shape}')
+
+
+# In[10]:
+
+
+import keras
+from keras.layers import Dense, Conv2D
+from keras.layers import Flatten
+from keras.layers import MaxPooling2D, GlobalAveragePooling2D
+from keras.layers import Activation
+from keras.layers import BatchNormalization
+from keras.layers import Dropout
+from keras.models import Sequential
+from keras import backend as K
+
+from keras import optimizers
+
+
+# In[13]:
+
+
+def CNNbuild(height, width, classes, channels):
+    model = Sequential()
+
+    chanDim = -1
+    model.add(Conv2D(32, kernel_size = (5,5),  strides=(1, 1), activation = 'relu', input_shape = (50, 50, 3)))
+    model.add(MaxPooling2D(2,2))
+    model.add(BatchNormalization(axis = chanDim))
+    model.add(Dropout(0.25))
+    
+    model.add(Conv2D(16, kernel_size = (5,5), strides=(1, 1), activation = 'relu'))
+    model.add(MaxPooling2D(2,2))
+    model.add(Dropout(0.25))
+    model.add(BatchNormalization(axis = chanDim))
+    model.add(Flatten())
+    
+    model.add(Dense(150, activation = 'relu'))
+    
+    model.add(Dense(84, activation = 'relu'))
+    model.add(Dropout(0.4))
+    model.add(Dense(classes, activation = 'sigmoid'))    
+    inputShape = (height, width, channels)
+
+    
+    return model
+
+
+# In[14]:
+
+
+#instantiate the model
+height = 50
+width = 50
+classes = 2
+channels = 3
+model = CNNbuild(height = height, width = width, classes = classes, channels = channels)
+model.summary()
+
+
+# In[15]:
+
+
+model.compile(loss = 'categorical_crossentropy', optimizer = 'Adam', metrics = ['accuracy'])
+
+
+# In[16]:
+
+
+h = model.fit(x_train, y_train, epochs = 20, batch_size = 32)
+
+
+# In[17]:
+
+
+plt.figure(figsize = (18,8))
+plt.plot(range(20), h.history['acc'], label = 'Training Accuracy')
+plt.plot(range(20), h.history['loss'], label = 'Taining Loss')
+#ax1.set_xticks(np.arange(0, 31, 5))
+plt.xlabel("Number of Epoch's")
+plt.ylabel('Accuracy/Loss Value')
+plt.title('Training Accuracy and Training Loss')
+plt.legend(loc = "best")
+
+
+# In[18]:
+
+
+predictions = model.evaluate(x_test, y_test)
+print(f'LOSS : {predictions[0]}')
+print(f'ACCURACY : {predictions[1]}')
+
